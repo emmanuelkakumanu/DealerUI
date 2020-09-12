62d44d0d836c47b8d06a98575a69c7f74171715e
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2020-09-12 19:31:41 +0530

    Add files via upload

---
+++ EurekaDiscoveryService/src/test/java/com/cts/EurekaDiscoveryService/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.EurekaDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
